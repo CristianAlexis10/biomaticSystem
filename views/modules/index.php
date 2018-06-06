c2cb037a68505c22b274a5e1a0be91c806c4b02e
evelinFrontend <ginatha97@gmail.com>
2018-06-06 17:17:19 -0500

    login

---
+++ views/modules/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Biomatic</title>
+    <link rel="stylesheet" href="views/assets/css/login.css">
+  </head>
+  <body>
+    <div class="container--modal">
+      <img src="views/assets/image/logo.png" alt="logo">
+      <form  id="formLogin">
+        <div class="form--group">
+          <label for="email" class="login--label">correo:</label>
+          <input type="email" id="email" class="login--input">
+        </div>
+        <div class="form--group">
+          <label for="password" class="login--label">contraseña:</label>
+          <input type="password" id="password" class="login--input">
+        </div>
+        <div class="form--group">
+          <input type="submit" value="ingresar" class="btn">
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
