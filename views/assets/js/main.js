6366797f7d7e3c6e7002361b3a36a27e2fc50d23
CristianAlexis10 <cristian1020011@gmail.com>
2018-06-06 23:23:47 -0500

    sadsa

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -26,6 +26,42 @@ $("#formLogin").submit(function(e){
     setTimeout(function(){$("div.message").remove();},4000);
   }
 });
+$("#createUser").submit(function(e){
+  e.preventDefault();
+  if ($("#nameUser").val() != "" && $("#SnameUser").val() != "" && $("#lastnameUser").val() != "" && $("#SlastnameUser").val() != ""  && $("#emailUser").val() != ""  && $("#passUser").val() != ""  && $("#repPassUser").val() != "") {
+      var data= {};
+      data.push($("#nameUser").val());
+      data.push($("#SnameUser").val());
+      data.push($("#lastnameUser").val());
+      data.push($("#SlastnameUser").val());
+      data.push($("#emailUser").val());
+      data.push($("#passUser").val());
+    // $.ajax({
+    //   url:"crear-usuario",
+    //   type:"post",
+    //   dataType:"json",
+    //   data:({user:$("#email").val() , pass:$("#password").val()}),
+    //   success:function(result){
+    //     console.log(result);
+    //     if (result=="admin") {
+    //       location.href="adminstracion";
+    //     }else if(result=="user"){
+    //       location.href="user";
+    //     }else{
+    //       $("div.message").remove();
+    //       $("#formLogin").after("<div class='message'>"+result+"</div>");
+    //       setTimeout(function(){$("div.message").remove();},4000);
+    //     }
+    console.log(data);
+    //   },
+    //   error:function(result){console.log(result);}
+    // });
+  }else{
+    $("div.message").remove();
+    $("#createUser").after("<div class='message'>Todos los campos son requeridos.</div>");
+    setTimeout(function(){$("div.message").remove();},4000);
+  }
+});
 
 $( function() {
   $("#tabs").tabs();
