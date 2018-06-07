b9357d285a7247d3749eb360f848f233bebc5db0
evelinFrontend <ginatha97@gmail.com>
2018-06-07 13:45:42 -0500

    serg

---
+++ views/modules/admin/groups/detail.php
@@ -0,0 +1,41 @@
+<h1 class="title">nombre del grupo</h1>
+<p>fecha de creacion</p>
+<p>descripcion del grupo</p>
+<div class="content--miembro">
+  <p>miembros del grupo</p>
+  <table class="table--detail">
+    <thead>
+      <tr>
+        <th>nombre</th>
+        <th>apellido</th>
+        <th>nose</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>evelin</td>
+        <td>lopera</td>
+        <td>nose</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+<div class="content--proyecto">
+  <p>proyectos asiganados</p>
+  <table class="table--detail">
+    <thead>
+      <tr>
+        <th>nombre</th>
+        <th>descripcion</th>
+        <th>nose</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>evelin</td>
+        <td>sdvasdvasdv</td>
+        <td>nose</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
