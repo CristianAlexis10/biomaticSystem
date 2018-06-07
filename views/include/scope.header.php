3cc586e351dea73995cc25de55c3d9f9b07e37ee
evelinFrontend <ginatha97@gmail.com>
2018-06-06 21:26:44 -0500

    gd

--- views/include/scope.header.php
+++ views/include/scope.header.php
@@ -2,6 +2,10 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Biomatic</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="shortcut icon" href="views/assets/image/icn-maxi.png">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="views/assets/css/main.css">
   </head>
   <body>
