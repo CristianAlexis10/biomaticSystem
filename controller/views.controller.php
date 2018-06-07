f2b6c29cfe7a3badb2fceb581a017fec37588e3b
CristianAlexis10 <cristian1020011@gmail.com>
2018-06-06 22:12:11 -0500

    asdsa

--- controller/views.controller.php
+++ controller/views.controller.php
@@ -9,5 +9,15 @@
 		function main(){
 			require_once "views/modules/index.php";
 		}
+		function users(){
+			if (isset($_SESSION['USER']['ROL']) && $_SESSION['USER']['ROL'] == 1) {
+				require_once "views/include/scope.header.php";
+				require_once "views/modules/admin/user/user.php";
+				require_once "views/include/scope.footer.php";
+			}else{
+				session_destroy();
+				header("Location: inicio");
+			}
+		}
 	}
 ?>
