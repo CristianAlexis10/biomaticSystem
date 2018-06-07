08e080a64386dd0a56ebc42c9da3126119886ed9
evelinFrontend <ginatha97@gmail.com>
2018-06-06 21:27:34 -0500

    Merge branch 'master' of https://github.com/CristianAlexis10/biomaticSystem

--- biomatic_proyectos.sql
+++ biomatic_proyectos.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-06-2018 a las 03:24:01
+-- Tiempo de generación: 07-06-2018 a las 04:05:37
 -- Versión del servidor: 10.1.8-MariaDB
 -- Versión de PHP: 5.6.14
 
@@ -43,6 +43,116 @@ INSERT INTO `acceso` (`acc_token`, `usu_codigo`, `acc_contra`, `acc_codigo`) VAL
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `archivos`
+--
+
+CREATE TABLE `archivos` (
+  `arc_codigo` int(11) NOT NULL,
+  `arc_nombre` varchar(40) NOT NULL,
+  `arc_descripcion` longtext NOT NULL,
+  `tip_arc_codigo` int(11) NOT NULL,
+  `arc_url` varchar(200) NOT NULL,
+  `arc_fecha` date NOT NULL,
+  `pro_codigo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupos`
+--
+
+CREATE TABLE `grupos` (
+  `gru_codigo` int(11) NOT NULL,
+  `gru_nombre` varchar(50) NOT NULL,
+  `gru_descripcion` longtext NOT NULL,
+  `gru_fecha_resgistro` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `programa_formacion`
+--
+
+CREATE TABLE `programa_formacion` (
+  `prog_codigo` int(11) NOT NULL,
+  `porg_nombre` varchar(130) NOT NULL,
+  `prog_siglas` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyecto`
+--
+
+CREATE TABLE `proyecto` (
+  `pro_codigo` int(11) NOT NULL,
+  `pro_nombre` varchar(50) NOT NULL,
+  `pro_inicio` date NOT NULL,
+  `pro_programa_formacion` int(11) NOT NULL,
+  `tip_pro_codigo` int(11) NOT NULL,
+  `pro_serial` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyectoxgrupo`
+--
+
+CREATE TABLE `proyectoxgrupo` (
+  `pro_codigo` int(11) NOT NULL,
+  `gru_codigo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_archivo`
+--
+
+CREATE TABLE `tipo_archivo` (
+  `tip_arc_codigo` int(11) NOT NULL,
+  `tip_arc_nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tipo_archivo`
+--
+
+INSERT INTO `tipo_archivo` (`tip_arc_codigo`, `tip_arc_nombre`) VALUES
+(1, 'Proyecto'),
+(2, 'Acta de Reunión'),
+(3, 'Listado de Asistencia a Reuniones'),
+(4, 'Informe de investigación'),
+(5, 'Articulo como resultado de la investigación'),
+(6, 'Articulos de estudio');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_proyecto`
+--
+
+CREATE TABLE `tipo_proyecto` (
+  `tip_pro_codigo` int(11) NOT NULL,
+  `tip_pro_nombre` varchar(40) NOT NULL,
+  `tip_pro_descripcion` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tipo_proyecto`
+--
+
+INSERT INTO `tipo_proyecto` (`tip_pro_codigo`, `tip_pro_nombre`, `tip_pro_descripcion`) VALUES
+(1, 'Investigación', ''),
+(2, 'Innovaciòn', '');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -64,6 +174,18 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`usu_codigo`, `usu_nombre`, `usu_nombre2`, `usu_apellido`, `usu_apellido2`, `usu_correo`, `rol_id`, `usu_estado`) VALUES
 (1, 'Evelin', NULL, 'lopera', NULL, 'eve@gmail.com', 1, 'Activo');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarioxgrupo`
+--
+
+CREATE TABLE `usuarioxgrupo` (
+  `gru_codigo` int(11) NOT NULL,
+  `usu_codigo` int(11) NOT NULL,
+  `fecha_ingreso` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Índices para tablas volcadas
 --
@@ -76,6 +198,53 @@ ALTER TABLE `acceso`
   ADD KEY `usu_codigo` (`usu_codigo`);
 
 --
+-- Indices de la tabla `archivos`
+--
+ALTER TABLE `archivos`
+  ADD PRIMARY KEY (`arc_codigo`),
+  ADD KEY `tip_arc_codigo` (`tip_arc_codigo`),
+  ADD KEY `pro_codigo` (`pro_codigo`);
+
+--
+-- Indices de la tabla `grupos`
+--
+ALTER TABLE `grupos`
+  ADD PRIMARY KEY (`gru_codigo`);
+
+--
+-- Indices de la tabla `programa_formacion`
+--
+ALTER TABLE `programa_formacion`
+  ADD PRIMARY KEY (`prog_codigo`);
+
+--
+-- Indices de la tabla `proyecto`
+--
+ALTER TABLE `proyecto`
+  ADD PRIMARY KEY (`pro_codigo`),
+  ADD KEY `pro_programa_formacion` (`pro_programa_formacion`),
+  ADD KEY `tipo_proyecto` (`tip_pro_codigo`);
+
+--
+-- Indices de la tabla `proyectoxgrupo`
+--
+ALTER TABLE `proyectoxgrupo`
+  ADD KEY `pro_codigo` (`pro_codigo`),
+  ADD KEY `gru_codigo` (`gru_codigo`);
+
+--
+-- Indices de la tabla `tipo_archivo`
+--
+ALTER TABLE `tipo_archivo`
+  ADD PRIMARY KEY (`tip_arc_codigo`);
+
+--
+-- Indices de la tabla `tipo_proyecto`
+--
+ALTER TABLE `tipo_proyecto`
+  ADD PRIMARY KEY (`tip_pro_codigo`);
+
+--
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
@@ -83,9 +252,46 @@ ALTER TABLE `usuario`
   ADD KEY `rol_id` (`rol_id`);
 
 --
+-- Indices de la tabla `usuarioxgrupo`
+--
+ALTER TABLE `usuarioxgrupo`
+  ADD KEY `gru_codigo` (`gru_codigo`),
+  ADD KEY `usu_codigo` (`usu_codigo`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
+--
+-- AUTO_INCREMENT de la tabla `archivos`
+--
+ALTER TABLE `archivos`
+  MODIFY `arc_codigo` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `grupos`
+--
+ALTER TABLE `grupos`
+  MODIFY `gru_codigo` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `programa_formacion`
+--
+ALTER TABLE `programa_formacion`
+  MODIFY `prog_codigo` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `proyecto`
+--
+ALTER TABLE `proyecto`
+  MODIFY `pro_codigo` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tipo_archivo`
+--
+ALTER TABLE `tipo_archivo`
+  MODIFY `tip_arc_codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT de la tabla `tipo_proyecto`
+--
+ALTER TABLE `tipo_proyecto`
+  MODIFY `tip_pro_codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
@@ -100,6 +306,34 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `acceso`
   ADD CONSTRAINT `acceso_ibfk_1` FOREIGN KEY (`usu_codigo`) REFERENCES `usuario` (`usu_codigo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `archivos`
+--
+ALTER TABLE `archivos`
+  ADD CONSTRAINT `archivos_ibfk_1` FOREIGN KEY (`pro_codigo`) REFERENCES `proyecto` (`pro_codigo`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `archivos_ibfk_2` FOREIGN KEY (`tip_arc_codigo`) REFERENCES `tipo_archivo` (`tip_arc_codigo`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `proyecto`
+--
+ALTER TABLE `proyecto`
+  ADD CONSTRAINT `proyecto_ibfk_1` FOREIGN KEY (`pro_programa_formacion`) REFERENCES `programa_formacion` (`prog_codigo`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `proyecto_ibfk_2` FOREIGN KEY (`tip_pro_codigo`) REFERENCES `tipo_proyecto` (`tip_pro_codigo`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `proyectoxgrupo`
+--
+ALTER TABLE `proyectoxgrupo`
+  ADD CONSTRAINT `proyectoxgrupo_ibfk_1` FOREIGN KEY (`gru_codigo`) REFERENCES `grupos` (`gru_codigo`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `proyectoxgrupo_ibfk_2` FOREIGN KEY (`pro_codigo`) REFERENCES `proyecto` (`pro_codigo`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarioxgrupo`
+--
+ALTER TABLE `usuarioxgrupo`
+  ADD CONSTRAINT `usuarioxgrupo_ibfk_1` FOREIGN KEY (`usu_codigo`) REFERENCES `usuario` (`usu_codigo`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `usuarioxgrupo_ibfk_2` FOREIGN KEY (`gru_codigo`) REFERENCES `grupos` (`gru_codigo`) ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
